71d853dc0e0024e124478f6ca4dd4deb7e0b87e1
Ayush Tiwari <ayushzztiwari57@gmail.com>
2025-09-04 17:34:29 +0530

    Folder Struccture Added

---
+++ template.sh
@@ -0,0 +1,17 @@
+#CREATING DIRECTORIES
+mkdir -p src
+mkdir -p research
+
+#Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setuo.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+
+echo "Directory and files created successfully."
