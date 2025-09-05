f7a7b8968b914e1618c1661a3e23c6f72a6f9a07
Ayush Tiwari <ayushzztiwari57@gmail.com>
2025-09-05 14:29:18 +0530

    requiremernts added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="REMEDI",
+    version="0.1.0",
+    author="Ayush Tiwari",
+    author_email="ayushzztiwari69@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
